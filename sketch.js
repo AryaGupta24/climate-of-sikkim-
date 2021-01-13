a895f174bd86d80c8a06602b0e2dc2a95dfba40f
Arya Gupta <71432307+AryaGupta24@users.noreply.github.com>
2021-01-13 10:50:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+/*var s1, s1img
+
+function preload(){
+    createCanvas(1000,1000);
+    s1img = loadImage("weather.jpg");
+}
+
+function draw(){
+    background("white");
+    s1 = createSprite(600,600, 50, 50);
+    //s1.addImage(s1img);
+    drawSprites();
+}*/
